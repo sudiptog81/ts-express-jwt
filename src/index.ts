67491a8097e3e2fe719070637e596cf70976d845
Sudipto Ghosh <sudipto@ghosh.pro>
2019-05-20 16:36:05 +0530

    enabled CORS

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
-import bodyParser from "body-parser";
+import cors from "cors";
 import multer from "multer";
+import bodyParser from "body-parser";
 import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 const upload = multer({
     storage: multer.memoryStorage()
 });
-const port: number = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const port: number = process.env.PORT ? parseInt(process.env.PORT) : 8001;
+
+app.options("*", cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.any());
 
 app.get("/", (req: Request, res: Response): void => {
-    res.json({ message: "Su=cess" });
+    res.json({ message: "Success" });
 });
 
 app.post("/post", (req: Request, res: Response): void => {
