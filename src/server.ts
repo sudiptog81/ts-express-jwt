ef89b90f8bd4628a0eae94e23fc5e8d2f4814a24
Sudipto Ghosh <sudipto@ghosh.pro>
2019-05-26 10:18:35 +0530

    added a whitespace

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,5 @@ const port: number = process.env.PORT ? parseInt(process.env.PORT) : 8000;
 
 // Initialize DB Table
 createUsersTable();
+
 app.listen(port, (): void => console.log(`Started on localhost:${port}`));
